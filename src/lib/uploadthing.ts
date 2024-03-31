f941c785cb6a6358954699fde5bb80bbc5f28f31
AliSiddique <alisiddique10@hotmail.com>
2024-03-31 20:29:13 +0100

    SD

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -6,4 +6,4 @@ import {
 import type { OurFileRouter } from "../app/api/uploadthing/core";
  
 export const UploadButton = generateUploadButton<OurFileRouter>();
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>(); 
