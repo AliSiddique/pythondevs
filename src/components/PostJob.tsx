dbf8dc512db37e49676c0220cb7f84d39ed4b232
AliSiddique <alisiddique10@hotmail.com>
2024-03-31 20:26:28 +0100

    SD

--- src/components/PostJob.tsx
+++ src/components/PostJob.tsx
@@ -343,8 +343,7 @@ type Props = {};
         {/* <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
           <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg> */}
-  <UploadButton
-  className='bg-purple-500 text-white rounded-full h-20 w-20 hover:opacity-40 px-4 py-2'
+ <UploadButton
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
           // Do something with the response
--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -2,7 +2,6 @@ import {
   generateUploadButton,
   generateUploadDropzone,
 } from "@uploadthing/react";
-
  
 import type { OurFileRouter } from "../app/api/uploadthing/core";
  
