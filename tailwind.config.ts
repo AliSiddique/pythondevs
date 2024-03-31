d061e0a1aae0522dc8c813848004917d97747d7d
AliSiddique <alisiddique10@hotmail.com>
2024-03-31 20:14:08 +0100

    D

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss';
 const defaultTheme = require('tailwindcss/defaultTheme');
 
-const colors = require('tailwindcss/colors');
 const {
   default: flattenColorPalette,
 } = require('tailwindcss/lib/util/flattenColorPalette');
