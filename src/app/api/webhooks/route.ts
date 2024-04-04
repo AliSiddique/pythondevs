7e5d95ce00387e5950f9dc7915331baeab1cf9ce
AliSiddique <alisiddique10@hotmail.com>
2024-04-04 20:55:51 +0100

    SD

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -18,6 +18,7 @@ export async function POST(req: Request) {
       body,
       signature,
      "whsec_LrBk50FqowER6eTQx7KRrwWGYnsB0ixq"
+    // "whsec_9e116fa7a2674b1799fb066ef7e8b28a55c617cd7b402d5b899ea1fbebd9a540"
     );
   } catch (error: any) {
     return new Response(`Webhook Error: ${error.message}`, { status: 400 });
@@ -25,58 +26,31 @@ export async function POST(req: Request) {
 
   const session = event.data.object as Stripe.Checkout.Session;
 
-  if (event.type === 'checkout.session.completed') {
-    // Retrieve the subscription details from Stripe.
- 
-      // metadata is the data we passed in the checkout session.
-      return new Response(null, { status: 200 });
-    // const job_id = session.metadata?.job_id;
-    // if (!job_id) {
-    //   return new Response(null, { status: 400 });
-    // }
-    // const jobsPost = await db.jobPost.findUnique(
-    //   {
-    //     where: {
-    //       id: parseInt(job_id)
-    //     }
-    //   }
-    // )
-    // console.log(jobsPost)
-    // if(!jobsPost) {
-    //   return new Response(null, { status: 404 });
-    // }
-    // jobsPost.featured = true
+  if (event.type === 'checkout.session.completed' || event.type === 'invoice.payment_succeeded') {
+    const job_id = session.metadata?.job_id;
+    if (!job_id) {
+        return new Response(null, { status: 400 });
+    }
+    const jobsPost = await db.jobPost.findUnique({
+        where: {
+            id: parseInt(job_id)
+        }
+    });
 
-    // Update the user stripe into in our database.
-    // Since this is the initial subscription, we need to update
-    // the subscription id and customer id.
-   
-  }
+    if (jobsPost) {
+        // Update the 'featured' value
+        jobsPost.featured = true;
 
-  if (event.type === 'invoice.payment_succeeded') {
-    // Retrieve the subscription details from Stripe.
-    return new Response(null, { status: 200 });
+        // Save the updated 'jobsPost' object back to the database
+        await db.jobPost.update({
+            where: { id: parseInt(job_id) },
+            data: {
+                featured: true
+            }
+        });
+    }
+}
 
-    // const job_id = session.metadata?.job_id
-    // if (!job_id) {
-    //   return new Response(null, { status: 400 });
-    // }
-    // const jobsPost = await db.jobPost.findUnique(
-    //   {
-    //     where: {
-    //       id: parseInt(job_id)
-    //     }
-    //   }
-    // )
-
-    // console.log(jobsPost)
-    // if(!jobsPost) {
-    //   return new Response(null, { status: 404 });
-    // }
-    // jobsPost.featured = true
-    // Update the price id and set the new period end.
-   
-  }
   if (event.type === 'checkout.session.expired') {
     // If the payment failed, we want to cancel the subscription.
     const { data, error } = await resend.emails.send({
